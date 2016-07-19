5be774ee67a236cd0ee467f2b0da6858eadf514b
Richard Steen <richard.steen@activenetwork.com>
2016-07-18 22:04:25 -0500

    cors

--- web/router.ex
+++ web/router.ex
@@ -19,7 +19,6 @@ defmodule PeepBlogBackend.Router do
     resources "/posts", PostController
   end
 
-  plug PlugCors, [origins: [System.get_env("FRONTEND_URL")]]
   # Other scopes may use custom stacks.
   # scope "/api", PeepBlogBackend do
   #   pipe_through :api
